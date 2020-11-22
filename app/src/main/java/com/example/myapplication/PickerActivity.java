9ad1ba6626cced0d8abc0b4c40b2fe61fd66a74d
Aga K <59511312+agnieszkaq@users.noreply.github.com>
2020-11-22 15:30:09 +0100

    Finish PickerActivity.

--- app/src/main/java/com/example/myapplication/PickerActivity.java
+++ app/src/main/java/com/example/myapplication/PickerActivity.java
@@ -1,14 +1,32 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class PickerActivity extends AppCompatActivity {
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picker);
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void onClick(View view) {
+        Toast.makeText(getBaseContext(),
+                "Time selected: " +
+                        timePicker.getHour() +
+                        ":" + timePicker.getMinute(),
+                Toast.LENGTH_SHORT).show();
     }
 }
