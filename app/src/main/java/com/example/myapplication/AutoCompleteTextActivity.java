81d88eef22c41d5b932b39679a05200f085d9cef
Aga K <59511312+agnieszkaq@users.noreply.github.com>
2020-11-22 15:04:59 +0100

    Finish AutoCompleteTextActivity.

--- app/src/main/java/com/example/myapplication/AutoCompleteTextActivity.java
+++ app/src/main/java/com/example/myapplication/AutoCompleteTextActivity.java
@@ -3,12 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class AutoCompleteTextActivity extends AppCompatActivity {
+
+    String[] shortNames = {
+            "Aga",
+            "Agu",
+            "Agula",
+            "Agusia",
+            "Aguśka",
+            "Agniesia",
+            "Agniecha",
+            "Agnieszka"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_complete_text);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,shortNames);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.AutoCompleteViewTxt);
+        textView.setThreshold(3);
+        textView.setAdapter(adapter);
+
     }
 }
